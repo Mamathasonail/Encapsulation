a468fc55c2b38377bc4237aa92605b0dc8d5ac04
Mamathasonail <123888158+Mamathasonail@users.noreply.github.com>
2023-07-01 11:50:20 +0530

    Add files via upload

---
+++ encaps.java
@@ -0,0 +1,13 @@
+package encapsulation;
+
+public class encaps {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		test t = new test();
+		t.setB(5);
+		t.getB();
+		System.out.println(t.getB());
+	}
+
+}
